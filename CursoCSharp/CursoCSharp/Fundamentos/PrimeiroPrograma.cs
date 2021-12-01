de7c3932d64bc44531f91952c86cbc5c4e8df52d
Luísa de Barros Senra <luisa@j4s.tech>
2021-12-01 14:18:53 -0300

    Primeira Aula

---
+++ CursoCSharp/CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    internal class PrimeiroPrograma {
+
+        // começo do progrma - Main()
+        public static void Executar() {
+            Console.WriteLine("só pra colocar alguma coisa");
+        }
+
+    }
+}
