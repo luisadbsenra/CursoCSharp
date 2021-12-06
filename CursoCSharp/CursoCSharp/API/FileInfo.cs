3a70751131d5712e0cd1a59644c4b578fcd7efbe
Luísa de Barros Senra <luisa@j4s.tech>
2021-12-06 15:48:41 -0300

    Curso Final

--- CursoCSharp/CursoCSharp/API/FileInfo.cs
+++ CursoCSharp/CursoCSharp/API/FileInfo.cs
@@ -1,13 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace CursoCSharp.API {
     internal class FileInfo {
         public static void Exe() {
             var caminhoOrigem = @"~/arq_origem.txt".ParseHome();
+            var caminhoDestino = @"~/arq_destino.txt".ParseHome();
+            var caminhoCopia = @"~/arq_copia.txt".ParseHome();
+
+            using (StreamWriter sw = File.CreateText(caminhoOrigem))
+            {
+                sw.WriteLine("Arquivo original!");
+            }
+
+            FileInfo fi = new FileInfo();
+
+
         }
     }
 }
